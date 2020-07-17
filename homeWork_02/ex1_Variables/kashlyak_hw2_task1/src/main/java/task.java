7d007abd7dff554ffc219b8f8209111c343eb218
Alexey Kashlyak <lexa.april@gmail.com>
2020-07-17 14:50:53 +0300

    hw2task1

---
+++ homeWork_02/ex1_Variables/kashlyak_hw2_task1/src/main/java/task.java
@@ -0,0 +1,68 @@
+package main.java;
+
+public class task {
+    public static void main(String[] args) {
+
+        // Колчиество кораблей в порту
+        int countShips;
+
+        //Счетчик целых чисел
+        int count;
+
+        //Первое число
+        int firstNumber;
+
+        //Ответ на вопрос
+        String answer;
+
+        //Случайное число
+        double randomNumber;
+
+        //Генератор случайных чисел
+        double sourceRandomNumber;
+
+        //Цвет автомобиля;
+        String carColor;
+
+        //Скорость движения автомобиля
+        double carSpeed;
+
+        //Введеное число пользователем
+        double consoleNumber;
+
+        //Наибольшее число
+        double max;
+
+        //Наименьшее число
+        double min;
+
+        //Левая граница диапазона чисел
+        double leftRange;
+
+        //Правая граница диапазона чисел
+        double rightRange;
+
+        //Загаданное пользователем число
+        double guessedNumber;
+
+        //Вариант ответа
+        String option;
+
+        //Банковский счет
+        long bankAccount;
+
+        //Банковский баланс
+        long bankBalance;
+
+        //Кредитная карта
+        int creditCard;
+
+        //Количество колес у велосипеда
+        short countBicycleWheel;
+
+        //Стоимость билета
+        double ticketPrice;
+
+
+    }
+}
