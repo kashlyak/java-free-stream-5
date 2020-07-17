c42a28fff43197c31770d238df18cf95f309a971
Alexey Kashlyak <lexa.april@gmail.com>
2020-07-17 16:08:54 +0300

    kashlyak_hw2_task4

---
+++ homeWork_02/ex4_RandomNumberGenerator/kashlyak_hw2_task4/src/main/java/RandomGeneratorDemo.java
@@ -0,0 +1,14 @@
+public class RandomGeneratorDemo {
+    public static void main(String[] args) {
+
+        double a = Math.random();
+        double b = Math.random();
+        double c = Math.random();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+        double sum = a + b + c;
+        System.out.println(sum);
+    }
+}
